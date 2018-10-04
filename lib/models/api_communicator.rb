cdc67ce5df0a1d25691b8a1e9cf22b044d2e44bc
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-03 23:38:47 -0400

    add to api_communicator for pulling down all data later

--- lib/models/api_communicator.rb
+++ lib/models/api_communicator.rb
@@ -9,4 +9,12 @@ require_all 'lib'
 #   response_string = RestClient.get('https://api.globalwinescore.com/globalwinescores/latest/?limit=10', headers)
 #   response_hash = JSON.parse(response_string)
 #    wines = response_hash["results"]
+  # create_wines(wines)
+# end
+
+# def create_wines(wines)
+#   wines.each do |wine|
+#     wine_url = 'https://www.globalwinescore.com/wine-score/' + wine["wine_slug"]
+#     Wine.create(name: wine['wine'],color: wine['color'], country: wine['country'],year: wine['vintage'],score: wine['score'], url: wine_url)
+#   end
 # end
