3fc72c99450eb8fa0ca7c12c462997332739a574
mx@E <mglxp5@gmail.com>
2018-10-04 16:02:55 -0400

    npc menu half build, remade base menu

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,25 +14,101 @@ class Game
   end
 
   def print_commands
+    puts "Commands:"
     # exit
    # help
    # create npc custom
    # create npc random
   end
 
-
   def create_npc_custom
-    puts "Enter [name][race][age]"
-    input = gets.chomp.split
+    print "Name : "
+    name = gets.chomp
+    print "Race : "
+    race = gets.chomp
+    print "Age  : "
+    age = gets.chomp
+
+    input = [name, race, age]
     Npc.create_custom(input)
   end
 
   def create_npc_random
-    Npc.create_random(@current_world.id)
+    if @current_world != nil
+      Npc.create_random(@current_world.id)
+    else
+      Npc.create_random(nil)
+    end
   end
 
-  def print_all_npcs
-    puts Npc.all
+  def print_all_npcs # TODO print more than just name?
+    all_npcs = Npc.all
+    all_npcs.each {|n| puts n.name}
+  end
+
+  def print_npc_by_name(name)
+    like_name = "%#{name}%"
+    npc = Npc.where("name LIKE ?", like_name)
+    # TODO when npc has multiple npcs in returned to it
+    if !npc.empty?
+      puts npc.first.name
+    else
+      puts "Could not find #{name}"
+    end
+  end
+
+  def world_menu(cmd) # cmd is an array
+    case cmd[1]
+    when "create";  # custom;
+    when "print";   # attributes, towns;
+    when "modify";  # by name; --> modify menu
+    when "remove";  # by name;
+
+    when "switch";  # --> switch world menu
+    end
+  end
+
+  def town_menu(cmd) # cmd is an array
+    case cmd[0]
+    when "create";  # custom, random;
+    when "print";   # attributes, citizens;
+    when "modify";  # by name; --> modify menu
+    when "remove";  # by name;
+    end
+  end
+
+  def npc_menu(cmd) # cmd is an array
+    case cmd[0]
+    when "create";  # custom, random;
+      case cmd[1]
+      when "custom"; create_npc_custom;
+      when "random"; create_npc_random;
+      end
+    when "print";   # all, by name;
+      case cmd[1]
+      when "all"; print_all_npcs;
+      else; print_npc_by_name(cmd[1]);
+      end
+    when "modify";  # by name; --> modify menu
+    when "remove";  # by name;
+    else; # User can enter "npc #{name} #{command}"
+    end
+  end
+
+  def do_command_1(cmd)
+    split_cmd = cmd.downcase.split
+    first_cmd = cmd.first
+    rmain_cmd = split_cmd[1..split_cmd.size] # Remaining Commands
+
+    case split_cmd[0]
+    when "pry";   binding.pry;
+    when "exit";  @running = false;
+    when "help";  print_commands;
+    when "world"; world_menu(rmain_cmd); # TODO STOPPED HERE
+    when "town";  town_menu(rmain_cmd);
+    when "npc";   npc_menu(rmain_cmd);
+    else; puts "#{cmd} is not a valid command."
+    end
   end
 
   def do_command(cmd)
@@ -90,7 +166,7 @@ class Game
     while running
       print "Enter command: "
       cmd = gets.chomp
-      do_command(cmd)
+      do_command_1(cmd)
     end
   end
 
